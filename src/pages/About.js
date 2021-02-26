1586f4a6859a197192ed6aa8839e29a840358099
alihusnain4190 <alihusnain4190@gmail.com>
2021-02-26 12:50:36 +0000

    set router for all pages and add Hero component in home page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function About() {
-  return <h1>hello from about page</h1>;
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi veniam
+        reiciendis numquam porro, ullam voluptates nihil, totam illo placeat
+        vitae nulla non, exercitationem minus rerum debitis quas quaerat
+        corporis quam excepturi consequuntur sed dignissimos! Et quidem
+        accusamus quam suscipit, vel, nostrum totam veritatis eveniet debitis
+        ipsum at blanditiis tenetur odit!
+      </p>
+    </section>
+  );
 }
