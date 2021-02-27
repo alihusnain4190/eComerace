534f8eebe730e0dc68a60a5324a867145c1353d3
alihusnain4190 <alihusnain4190@gmail.com>
2021-02-27 13:49:43 +0000

    complete context and Empty cart

--- src/context/cart.js
+++ src/context/cart.js
@@ -1,1 +1,17 @@
 // cart context
+import React, { useState } from "react";
+import localCart from "../utils/localCart";
+const CartContext = React.createContext();
+function CartProvider({ children }) {
+  const [cart, setCart] = useState([]);
+
+  const [total, setToal] = useState(0);
+  const [cartItem, setCartItem] = useState(0);
+
+  return (
+    <CartContext.Provider value={{ cart, total, cartItem }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+export { CartContext, CartProvider };
